534b26698237d8560a2fc5d034b33cce0fcf76de
Jordana Amorim <jordana_amorim@hotmail.com>
2021-12-05 23:21:33 -0300

    final styling and implementation validations

--- app.js
+++ app.js
@@ -1,7 +1,72 @@
+//Accordion
 const accordions = document.querySelectorAll(".accordion-item");
 
 accordions.forEach(accordion => {
     accordion.addEventListener("click", () => {
         accordion.classList.toggle("active");
-    })  
+    })
 })
+
+//Form
+let error = false
+
+document.querySelector("#selectType")
+    .addEventListener('change', (event) => {
+        console.log(event)
+        event.isTrusted
+            ? document.querySelector("#requiredSelectType").innerHTML = ''
+            : ''
+
+        error = false
+    });
+
+document.querySelector("#selectArea")
+    .addEventListener('change', (event) => {
+        event.isTrusted
+            ? document.querySelector("#requiredSelectArea").innerHTML = ''
+            : ''
+
+        error = false
+    });
+
+document.querySelector("#selectVacancy")
+    .addEventListener('keydown', (event) => {
+        console.log(event.key)
+        event.key
+        ?  document.querySelector("#requiredSelectVacancy").innerHTML = ''
+        : ''
+
+        error = false
+    });
+
+document.querySelector("#search").addEventListener("click", (e) => {
+    let selectType = document.querySelector("#selectType").value;
+    let selectArea = document.querySelector("#selectArea").value;
+    let selectVacancy = document.querySelector("#selectVacancy").value;
+
+    let required = `<p class="required" >Campo Obrigatório</p>`;
+    let success = `<p class="required" >Buscando vagas</p>`;
+
+    if (selectType || selectArea || selectVacancy === '') {
+        if (selectType === "") {
+            document.querySelector("#requiredSelectType").innerHTML = required;
+            error = true
+        }
+
+        if (selectArea === "") {
+            document.querySelector("#requiredSelectArea").innerHTML = required;
+            error = true
+        }
+
+        if (selectVacancy === "") {
+            document.querySelector("#requiredSelectVacancy").innerHTML = required;
+            error = true
+        }
+    } else {
+        error = false
+    }
+
+    if (!error) {
+        document.querySelector("#sendform").innerHTML = success;
+    }
+});
